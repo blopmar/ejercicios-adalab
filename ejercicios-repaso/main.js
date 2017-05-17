ef74886ba2868fd3145c7a85bdfc33e1cb7c64a4
blopmar <be.lop.mar@gmail.com>
2017-05-17 23:22:09 +0200

    Ejercicios repaso JavaScript 1.1 (sin notas innecesarias)

--- ejercicios-repaso/main.js
+++ ejercicios-repaso/main.js
@@ -3,20 +3,6 @@ Ejercicio 1.
 Creamos una función que tomará como parámetros un número y un array de números.
 La función va a devolver el número de veces que ese número está repetido en el array.
 */
-/*var targetNumber = 6;
-var result = 0;
-
-function repeated (targetNumber) {
-for(var i=0; i<arrayNumbers.length; i++){
-  if(arrayNumbers[i] === targetNumber) {
-    result = result ++ 1;
-    }
-  }
-}
-return result;
-}
-
-console.log(repeated(targetNumber));*/
 
 var arrayNumbers = [1, 6, 8, 2, 6, 5, 4, 6];
 var targetNumber = 6;
