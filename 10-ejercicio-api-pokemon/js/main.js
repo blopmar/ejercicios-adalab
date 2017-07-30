4b988991b5beacee9ec2c8b14c53287b61867736
Beatriz López <be.lop.mar@gmail.com>
2017-07-31 01:28:20 +0200

    Add 'About' HTML and styles

--- 10-ejercicio-api-pokemon/js/main.js
+++ 10-ejercicio-api-pokemon/js/main.js
@@ -25,14 +25,14 @@ function refreshWeb(request){
   if(request.status >= 200 && request.status < 400) {
     var pokemon = JSON.parse(request.responseText);
     var container = document.querySelector('.cards-container');
+    container.innerHTML += '<p><img src="' + pokemon.sprites.front_default +'"></p>';
     container.innerHTML += '<p>Nombre: ' + pokemon.name +'</p>';
     container.innerHTML += '<p>Nº: ' + pokemon.id +'</p>';
-    container.innerHTML += '<p><img src="' + pokemon.sprites.front_default +'"></p>';
     var types = pokemon.types;
     if(types.length === 1){
       container.innerHTML += '<p>Tipo: ' + types[0].type.name +'</p>';
     } else {
-      container.innerHTML += '<p>Tipo: ' + types[0].type.name + ' / ' + types[1].type.name +'</p>';
+      container.innerHTML += '<p>Tipos: ' + types[0].type.name + ' / ' + types[1].type.name +'</p>';
     }
   } else {
     console.log('Error del servidor, puede que el archivo no exista o que se haya producido un error interno en el servidor');
